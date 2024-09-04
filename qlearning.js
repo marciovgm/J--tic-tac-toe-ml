6142f6c33fd333f88e45e146391d1f6fc85cb3b0
Márcio Milan <63435965+marciovgm@users.noreply.github.com>
2024-09-03 21:54:43 -0300

    Create qlearning.js

---
+++ qlearning.js
@@ -0,0 +1,125 @@
+class QLearningTicTacToe {
+    constructor() {
+        this.qTable = {};
+        this.learningRate = 0.1;
+        this.discountFactor = 0.9;
+        this.explorationRate = 0.1;
+        this.board = Array(9).fill(null);
+        this.player = 'X';
+        this.opponent = 'O';
+    }
+
+    getBoardState() {
+        return this.board.join('');
+    }
+
+    getAvailableMoves() {
+        return this.board.map((cell, index) => cell === null ? index : null).filter(val => val !== null);
+    }
+
+    chooseMove() {
+        if (Math.random() < this.explorationRate) {
+            return this.getAvailableMoves()[Math.floor(Math.random() * this.getAvailableMoves().length)];
+        } else {
+            const state = this.getBoardState();
+            if (!this.qTable[state]) this.qTable[state] = Array(9).fill(0);
+            let maxQ = Math.max(...this.qTable[state]);
+            let moves = this.getAvailableMoves().filter(index => this.qTable[state][index] === maxQ);
+            return moves[Math.floor(Math.random() * moves.length)];
+        }
+    }
+
+    updateQTable(reward) {
+        const state = this.getBoardState();
+        if (!this.qTable[state]) this.qTable[state] = Array(9).fill(0);
+        const move = this.lastMove;
+        const nextState = this.board.join('');
+        let maxNextQ = Math.max(...(this.qTable[nextState] || Array(9).fill(0)));
+        this.qTable[state][move] += this.learningRate * (reward + this.discountFactor * maxNextQ - this.qTable[state][move]);
+    }
+
+    makeMove(index) {
+        if (this.board[index] === null) {
+            this.board[index] = this.player;
+            this.lastMove = index;
+            if (this.checkWin(this.player)) {
+                this.updateQTable(1);
+                this.reset();
+            } else if (this.getAvailableMoves().length === 0) {
+                this.updateQTable(0.5);
+                this.reset();
+            } else {
+                this.opponentMove();
+            }
+        }
+    }
+
+    opponentMove() {
+        const opponentMove = this.getAvailableMoves()[Math.floor(Math.random() * this.getAvailableMoves().length)];
+        this.board[opponentMove] = this.opponent;
+        if (this.checkWin(this.opponent)) {
+            this.updateQTable(-1);
+            this.reset();
+        }
+    }
+
+    checkWin(player) {
+        const winConditions = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+        return winConditions.some(condition => condition.every(index => this.board[index] === player));
+    }
+
+    reset() {
+        this.board.fill(null);
+        this.renderBoard();
+    }
+
+    resetLearning() {
+        this.qTable = {};
+    }
+
+    async trainAgent(iterations = 100000) {
+        for (let i = 0; i < iterations; i++) {
+            this.board.fill(null);
+            while (this.getAvailableMoves().length > 0 && !this.checkWin(this.player) && !this.checkWin(this.opponent)) {
+                this.makeMove(this.chooseMove());
+                if (!this.checkWin(this.player) && this.getAvailableMoves().length > 0) {
+                    this.opponentMove();
+                }
+            }
+        }
+        alert('Treinamento concluído!');
+        this.reset();
+    }
+
+    renderBoard() {
+        const boardElement = document.getElementById('board');
+        boardElement.innerHTML = '';
+        this.board.forEach((cell, index) => {
+            const cellElement = document.createElement('div');
+            cellElement.className = 'cell';
+            cellElement.innerText = cell || '';
+            cellElement.addEventListener('click', () => this.makeMove(index));
+            boardElement.appendChild(cellElement);
+        });
+    }
+}
+
+const game = new QLearningTicTacToe();
+game.renderBoard();
+
+function resetGame() {
+    game.reset();
+}
+
+function resetLearning() {
+    game.resetLearning();
+    alert('Aprendizado zerado!');
+}
+
+function trainAgent() {
+    game.trainAgent();
+}
