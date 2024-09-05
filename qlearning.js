e121f564df997e7aab3833db58acf5c105684305
Márcio Milan <63435965+marciovgm@users.noreply.github.com>
2024-09-04 21:43:38 -0300

    Update qlearning.js

--- qlearning.js
+++ qlearning.js
@@ -102,7 +102,7 @@ class QLearningTicTacToe {
         while (i < iterations) {
             this.reset();
             alert('dentro');
-            alert(this.getAvailableMoves().length.toString());
+            alert('teste' + this.getAvailableMoves().length.toString());
             while (this.getAvailableMoves().length > 0 && !this.checkWin(this.player) && !this.checkWin(this.opponent)) {
                 alert('dentro 2');
                 this.makeMove(this.chooseMove());
